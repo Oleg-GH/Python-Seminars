72e1304ba645a3009b1ba19863f940715eaaf923
Oleg-GH <fan2004g@bk.ru>
2022-12-27 19:46:08 +0500

    Создайте словарь, заданный по формуле 3*n+1, где n это ключ, а формула значение. Пример: - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}

--- Sem2312_task1.py
+++ Sem2312_task1.py
@@ -3,11 +3,12 @@
 
 my_dict = {}
 
-number = int(input('Введите целое число: '))
+number = int(input('Введите целое число:  '))
 
 for n in range(1, number + 1):
     my_dict[n] = 3 * n + 1
 
-print(my_dict.get(7, 'Такого ключа нет'))    
+print(my_dict.get(7, 'Такого ключа нет'))   
+
 
 
