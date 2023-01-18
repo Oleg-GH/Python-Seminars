b05b25415df048537b46077edf4ea080fd0efa80
Oleg-GH <fan2004g@bk.ru>
2023-01-18 23:56:00 +0500

    Дан список чисел. Создайте список, в который попадают числа, образующие возрастающую последовательность. Порядок элементов менять нельзя. Пример: [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 2, 3, 4]

---
+++ Sem1301_increase.py
@@ -0,0 +1,23 @@
+# Дан список чисел. Создайте список, в который попадают числа, образующие возрастающую последовательность.
+# Порядок элементов менять нельзя. Пример: [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 2, 3, 4] и т.д.
+
+
+data = [1, 5, 2, 3, 4, 6, 1, 7]
+result = []
+count = 0
+num1 = 1
+while count < len(data):
+    for i in range(len(data) - 1):
+        temp = []
+        temp.append(data[i])
+        cur_max = data[i]
+        for j in range(num1, len(data)):
+            if data[j] > cur_max:
+                temp.append(data[j])
+                cur_max = data[j]
+        if len(temp) > 1 and temp not in result:
+            result.append(temp)
+    count += 1
+    num1 += 1    
+
+print(result)                
