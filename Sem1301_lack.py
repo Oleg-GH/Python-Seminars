fd38270a2b694c7cc5ffc35a97415537d228456d
Oleg-GH <fan2004g@bk.ru>
2023-01-18 23:54:18 +0500

    В файле находится N натуральных чисел, записанных через пробел. Среди чисел не хватает одного, чтобы выполнялось условие A[i] - 1 = A[i - 1]. Найдите это число.

---
+++ Sem1301_lack.py
@@ -0,0 +1,16 @@
+# В файле находится N натуральных чисел, записанных через пробел.
+# Среди чисел не хватает одного, чтобы выполнялось условие
+# A[i] - 1 = A[i - 1]. Найдите это число.
+
+data = '1 2 3 4 5 6 8 9 11 12 13 14 15 17 18 19 21'
+# data = [int(x) for x in data.split()]     # можно и так
+data = list(map(int, data.split()))
+
+for i in range(len(data) - 1):                  # это один способ
+    if not data[i] + 1 == data[i + 1]:
+        print(data[i] + 1)
+                                                # это второй способ
+# my_func = list(filter(lambda i: not data[i] + 1 == data[i + 1], range(len(data) - 1)))
+
+# for item in my_func:
+#     print(data[item] + 1)
